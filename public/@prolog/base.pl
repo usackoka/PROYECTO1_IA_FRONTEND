9129ce56793223832d7ac91e4638cbfd19e9fe07
Oscar René <usackoka@gmail.com>
2020-09-26 19:40:29 -0600

    Update base.pl

--- public/@prolog/base.pl
+++ public/@prolog/base.pl
@@ -28,7 +28,7 @@ animal(pez_payaso_comun,pez,[0.7,0.1,10,500000,1,marino]).
 animal(caracol,molusco,[1,3,9,1000000,10,marino]).
 animal(cangrejo,molusco,[0.5,10,20,1000000,0.8,terrestre]).
 animal(pulpo,molusco,[5,50,50,1000000,10,marino]).
-animal(medusa,molusco,[5,20,1000000,10,marino]).
+animal(medusa,molusco,[3,20,20,1000000,9,marino]).
 animal(almeja,molusco,[0.1,1,20,1000000,0,terrestre]).
 
 %aves
